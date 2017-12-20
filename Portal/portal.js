a9ece7233ee8125891df934e683de25ec18dd896
omajoshi9 <omajoshi9@gmail.com>
2017-12-19 22:04:43 -0600

    fixed
    yay

--- Portal/portal.js
+++ Portal/portal.js
@@ -1,12 +1,11 @@
-b = angular.element('[name=loginButton]').get(0)
-f = angular.element(b).scope();
+f = angular.element('[name=loginButton]').scope();
 l = f.model.LoginCredentials;
 l.UserType = "2";
 l.UserName = "ojoshi7782";
 l.Password = "0957782";
 l.RememberMe = true;
-f.model.PasswordHints.forEach(function(a) {
-    a.Response = "None"
-});
+f.model.PasswordHints.forEach(function(a){a.Response = "None"});
 f.$apply();
-b.click()
+if ($('.form-horizontal.ng-pristine.ng-valid.ng-hide').length) {
+    $('[name=loginButton]').get(0).click();
+}
