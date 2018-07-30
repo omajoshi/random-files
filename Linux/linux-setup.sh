9f84ebebcceca878249b519297fd07bed1e0154a
omajoshi9 <omajoshi9@gmail.com>
2018-07-30 13:44:57 -0500

    Update linux-setup.sh

--- Linux/linux-setup.sh
+++ Linux/linux-setup.sh
@@ -16,7 +16,7 @@ sudo usermod -aG sudo $user
 # part 2 in the README file
 sudo install -d -o $user -g $user -m 700 /home/$user/.ssh/
 cd /home/$user/.ssh/
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAjRsSi/vN2E4aZTDEPhF0/R/Q69BAIh+6ThiAVGT/SXF0wjlLbM+U8dd7qj1lCuEGKAOfj4gasePnKda3v1+PKDtYVQ36Qp1ZqQMj0afNnuTQFpi6+Aa+hoMtylVYCxXci+qXlrdPz4mMSdBnji+1oYp2+Qazc8L2MTQz7Ji71Xrv6hGiPNDjvV5037WikrwAsqMEMhZHKh53PNMlixpu9xiaivoXFqBC+mLos6Co0MH1QM2Y0p+GvQzhoThRafGUD1VLMU9MrNSbbg42khy5pTogB8MeHpc6YsVhgzwPNxf8HjcrAWNhCYI3NR96dyRdJA9bS59m16AwVMsYOmoqjw== rsa-key-20180730" | sudo cat > authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAoErgPvlJ+wsWp1yXJ1METBkqfQDdxQtg7KoaN5zfHQ1sOLgqAbwqnQqB4psjgQMtZljrjfJkFEvVtHnG6Qt2QGJghNpZQQdo9V28GDlbfO3dfO9dqn1EjEqU7xTqpwCxmr3P/sqCbzrAaWFq5mNMgv8xoa0A57Pb++Dx4oXgMw7Dq+cmCTT4bFODTcvkWrlUYcHFyuS1d4Mado607TsLX9/QHO+JgJFgyK3Snb8SkEwoVvuix1VYX75UGYRNnSy3pfI2z6roFa0i/s59WdwA+4UMeeldQN3XZMcN1/GUzN7Vsa7q8L6PWHFi/4DQ1UZqsP6sqMbEbh6ZJNR/diUuHQ== rsa-key-20180730" | sudo cat > authorized_keys
 sudo chown $user:$user authorized_keys
 sudo chmod 600 authorized_keys
 
