3c25e142cc96b170cd636e72dfa069f497d489bc
Om Joshi <omajoshi9@gmail.com>
2020-04-03 19:07:44 -0500

    Update linux-setup.sh

--- Linux/linux-setup.sh
+++ Linux/linux-setup.sh
@@ -29,10 +29,10 @@ sudo ufw --force enable
 
 # part 4
 sudo apt install python3.8 -y
-sudo ln -s /usr/local/bin/pip /usr/local/bin/pip3
-sudo ln -s /usr/bin/python3.8 /usr/local/bin/python3
-sudo ln -s /usr/bin/python3.8 /usr/local/bin/python
-sudo ln -s /usr/bin/python3.8 /usr/local/bin/py
+sudo ln -s -f /usr/local/bin/pip /usr/local/bin/pip3
+sudo ln -s -f /usr/bin/python3.8 /usr/local/bin/python3
+sudo ln -s -f /usr/bin/python3.8 /usr/local/bin/python
+sudo ln -s -f /usr/bin/python3.8 /usr/local/bin/py
 
 sudo apt install virtualenv -y
 
